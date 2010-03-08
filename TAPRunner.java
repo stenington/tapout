e1f6d49652f5eea24559172fe8f91dad1b719898
Mike Larsson <mikelarssonftw@gmail.com>
2010-03-08 02:02:13 -0500

    pass classname on commandline

--- TAPRunner.java
+++ TAPRunner.java
@@ -9,8 +9,17 @@ public class TAPRunner {
   public static void main(String[] args){
     JUnitCore core = new JUnitCore();
     core.addListener(new TAPListener());
-    Class[] classes = {ExampleTest.class};
-    core.run(classes);
+    Class[] classes = new Class[args.length];
+    ClassLoader loader = ClassLoader.getSystemClassLoader();
+    try {
+      for(int i = 0; i < args.length; i++){
+        classes[i] = loader.loadClass(args[i]);
+      }
+      core.run(classes);
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
   }
 
 }
